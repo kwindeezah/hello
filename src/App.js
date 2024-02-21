4b78d22af1e6e977d0a52ffd154712197cfbe5d8
kwindeezah <hadizathmotunrayo@gmail.com>
2024-02-21 18:09:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+class App extends React.Component() {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -13,13 +15,13 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
